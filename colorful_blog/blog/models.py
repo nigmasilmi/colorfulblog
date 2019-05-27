b6d154bcefcefd3b8fb00a3c9ba27c7b0fe087fa
Nigm4 <nigmasilmi.web@gmail.com>
2019-05-27 17:44:39 -0400

    still some issues with published and update

--- colorful_blog/blog/models.py
+++ colorful_blog/blog/models.py
@@ -4,45 +4,40 @@ from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
 
+
 class BlogPostQuerySet(models.QuerySet):
     def published(self):
-        print("entra a published")
         now = timezone.now()
         return self.filter(published_date__lte=now)
 
 
-
-
 class BlogPostManager(models.Manager):
-    def get_queryset(self):
+    def queryset(self):
         return BlogPostQuerySet(self.model, using=self._db)
 
     def published(self):
         return self.get_queryset().published()
+
 
 class BlogPost(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
-    published_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    published_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
+    objects = BlogPostManager()
 
-class Meta:
-    ordering: ['-published-date', '-updated', '-timestamp' ]
-
+    class Meta:
+        ordering: ['-published_date', '-updated', '-timestamp']
 
     def get_absolute_url(self):
-        print(str(self))
-        stringsub = f'/blog/blog{self.slug}'
-        print(stringsub)
-       # return F'/blog/{self.slug}'
-        return stringsub
+        return F'/blog/{self.slug}'
 
     def get_absolute_url_update(self):
-        return f'/blog/{self.slug}/edit'
+        return f'{self.get_absolute_url()}/edit'
 
     def get_absolute_url_delete(self):
-        return f'/blog/{self.slug}/delete'
+        return f'{self.get_absolute_url()}/delete'
--- colorful_blog/blog/views.py
+++ colorful_blog/blog/views.py
@@ -4,6 +4,7 @@ from .models import BlogPost
 from .forms import BlogPostModelForm
 
 
+
 @staff_member_required()
 def blog_post_create_view(request):
     obj = BlogPost()
@@ -13,9 +14,7 @@ def blog_post_create_view(request):
     if form.is_valid():
         post = form.save(commit=False)
         post.user = request.user
-        print("pasa por  despues de request user de create")
         post.save()
-        print("pasa por post save")
         # post = BlogPost.objects.create(**form.cleaned_data)
         # forma  propia django para instanciar un nuevo objeto desempaquetando el diccionario en key value
         # utilizando un modelform se simplifica con form.save
@@ -33,8 +32,7 @@ def blog_post_detail_view(request, slug):
 
 
 def blog_post_list_view(request):
-    qs = BlogPost.objects.all()
-    #qs = BlogPost.objects.all().published()
+    qs = BlogPost.objects.all().published()
     title = 'Blogposts'
     template_name = 'list.html'
     context = {'object_list': qs}
@@ -43,16 +41,11 @@ def blog_post_list_view(request):
 
 @staff_member_required()
 def blog_post_update_view(request, slug):
-    print("pasa por el principio de update")
     obj = get_object_or_404(BlogPost, slug=slug)
-    print("pasa por obj = get_object_or_404(BlogPost, slug=slug)")
     form = BlogPostModelForm(request.POST or None, instance=obj)
-    print("pasa por form = BlogPostModelForm(request.POST or None, instance=obj)")
+    print(form.errors)
     if form.is_valid():
-        print("pasa por if form.is_valid() comentado:")
         form.save()
-        print("pasa por form.save()")
-
     template_name = 'update.html'
     title = f"Edit {obj.title}"
     context = {'form': form, 'title': title}
@@ -69,4 +62,3 @@ def blog_post_delete_view(request, slug):
     template_name = 'delete.html'
     context = {'object': obj}
     return render(request, template_name, context)
-
